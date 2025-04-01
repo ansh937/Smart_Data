dcb9963a01c3c807b959cf034ccb7b5639e5d7fd
ansh937 <subedianish937@gmail.com>
2025-04-01 12:28:49 +0545

    firt commit

---
+++ example.py
@@ -0,0 +1,64 @@
+import math
+
+# math=3
+# math.real("1+3")
+# print(math)
+
+
+# a=4+6j
+# print(a.real)
+# print(a.imag)# to see imaginary number 
+
+# complex = complex(3)# function can be override
+# print(complex)
+
+# a=5;
+# print(math.ceil(a))
+# print(math.pow(a,3))
+
+# for i in range(0,40,2):
+#   print(i)
+
+# for i in range(10,-1,-1):
+#   print(i)
+
+l=[1,2,3,4,5,6]
+# print(len(l))
+
+# for i in range(len(l)):
+#   print(l[i])
+
+# l.pop()# removes the last element and returns it
+# print(l)
+
+
+# l.remove(4)#removes the 1st occurence of the element given as the argument
+# print(l)
+
+# del l[1] #deletes the element at a specific index
+# print(l)
+
+# l.append(6)#adds an element to the end of the list
+# print(l)
+
+# l.extend([2,3]) #adds all elements of the list given as the argument to the end of the origional list
+# print(l)
+
+
+# mat=[[1,2,3],[4,5,6],[7,8,9]]
+
+# print(mat[0][0])
+# print(mat[2][2])
+
+
+# def function_name(parameter1, parameter2):
+# 	# Function body using parameter1 and parameter2
+
+#? print(*objects, sep=' ', end='\n', file=None, flush=False)
+
+
+# print("aa",88,"67",7.990,sep="$")
+# print("anish", "manish", end="" )
+# print("  anish")
+
+
